84881079cb2d3452bf0c09befdedd9f48a8484b4
berry <berry3085@kimo.com>
2022-01-26 11:35:21 +0800

    Initial Commit

---
+++ Shared/EBook_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EBook_AppApp.swift
+//  Shared
+//
+//  Created by 沈秉叡 on 2022/1/26.
+//
+
+import SwiftUI
+
+@main
+struct EBook_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
